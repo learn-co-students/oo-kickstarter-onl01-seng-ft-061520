93d6b5fed51b7fe2f5af0e5af51d016e4145bff2
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-30 18:53:26 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,4 +1,14 @@
-class Backer 
-  
+class Backer
+  attr_reader :name, :backed_projects
 
-end 
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    backed_projects << project
+    project.backers << self
+  end
+
+end
--- lib/project.rb
+++ lib/project.rb
@@ -1,4 +1,14 @@
-class Project 
+class Project
+  attr_reader :title, :backers
 
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
 
-end 
+  def add_backer(backer)
+    backers << backer
+    backer.backed_projects << self
+  end
+
+end
