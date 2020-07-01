3e7726ce8a697dec430125fcdd17d009d135954b
Devon Wehrlin <dkwehrlin.99@gmail.com>
2020-07-01 19:36:29 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,5 +1,5 @@
 class Backer
-  attr_reader :name, :backed_projects
+  attr_reader :name, :backed_project
 
 
   def initialize(name)
@@ -9,7 +9,7 @@ class Backer
 
 def back_project(project)
   @backed_projects << project
-  project.backers << self 
+  project.backers << self
 end
 
 end
